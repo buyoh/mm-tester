ae92c205a0fe6dca7310ce0b79aacb9b0942748b
kosakkun <kosakkun0918@gmail.com>
2018-03-13 02:58:20 +0900

    制約を決めた

--- TSP/tester/Tester.java
+++ TSP/tester/Tester.java
@@ -1,15 +1,44 @@
 import java.awt.*;
+import javax.swing.JPanel;
+import java.security.*;
 
 public class Tester {
 
-    /********************************************************************/
-    static String seed, exec;
-    static boolean save, vis;
+    public class Visualizer extends JPanel {
+
+    }
 
     /********************************************************************/
+    static String fileName, exec;
+    static boolean save, vis, debug;
+
+    static final int maxN = 1000;
+    static final int minN = 20;
+    static final int Size = 100;
+
+    /********************************************************************/
+
+    public void generate (String seedStr) {
+        try {
+            SecureRandom rnd = SecureRandom.getInstance("SHA1PRNG");
+            long seed = Long.parseLong(seedStr);
+            rnd.setSeed(seed);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double runTest () {
+        return -1.0;
+    }
+
+    public Tester (String seed) {
+        generate(seed);
+        System.out.println(runTest());
+    }
 
     public static void main (String[] args) {
-        seed = "1";
+        String seed = "1";
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-seed")) {
             	seed = args[++i];
@@ -19,8 +48,12 @@ public class Tester {
                 vis = true;
             } else if (args[i].equals("-save")) {
                 save = true;
+            } else if (args[i].equals("-debug")) {
+                debug = true;
             }
         }
+        fileName = seed;
+        Tester test = new Tester(seed);
     }
 
 }
