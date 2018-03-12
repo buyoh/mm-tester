a6be8aff256e1e3c3f30e0b1fa50eb7dc3cc67c6
kosakkun <kosakkun0918@gmail.com>
2018-03-13 04:50:28 +0900

    スコアの計算とデバッグを実装

--- TSP/tester/Tester.java
+++ TSP/tester/Tester.java
@@ -22,7 +22,7 @@ public class Tester {
     static String fileName, exec;
     static boolean save, vis, debug;
 
-    final int MAXN = 200, MINN = 20;
+    final int MAXN = 30, MINN = 20;
     final int SIZE = 100;
     int N;
     int [] posX, posY;
@@ -37,11 +37,11 @@ public class Tester {
             long seed = Long.parseLong(seedStr);
             rnd.setSeed(seed);
 
-            N = rnd.nextInt(MAXN - MINN + 1) + MINN;
+            N = rnd.nextInt(MAXN - MINN) + MINN;
             perm = new int[N];
             posX = new int[N];
             posY = new int[N];
-            boolean [][] usedPos = new boolean[N][N];
+            boolean [][] usedPos = new boolean[SIZE][SIZE];
 
             for (int i = 0; i < N; ++i) {
                 int x = -1, y = -1;
@@ -55,7 +55,8 @@ public class Tester {
             }
 
             if (debug) {
-                System.out.println("N = " + N);
+                System.out.println("=================\nN = " + N);
+                System.out.println("=================\nposition");
                 for (int i = 0; i < N; ++i) {
                     System.out.println(posX[i] + " " + posY[i]);
                 }
@@ -68,13 +69,52 @@ public class Tester {
 
     }
 
-    public double runTest () {
+    public double runTest (String seed) {
+        try {
+            generate(seed);
+            if (proc != null) {
+                try {
+                    perm = getPermutation();
+                    boolean [] used = new boolean[N];
+                    for (int i = 0; i < N; ++i) {
+                        if (perm[i] < 0 || perm[i] >= N) {
+                            //addFatalError("All elements of your return must be between 0 and " + (N-1) + ", and your return contained " + perm[i] + ".");
+                            return -1;
+                        }
+                        if (used[perm[i]]) {
+                            //addFatalError("All elements of your return must be unique, and your return contained " + perm[i] + " twice.");
+                            return -1;
+                        }
+                        used[perm[i]] = true;
+                    }
+                } catch (Exception e) {
+                    //addFatalError("Failed to get result from permute.");
+                    return -1;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
 
-        return -1.0;
+        if (debug) {
+            System.out.println("=================\nresut");
+            for (int i = 0; i < perm.length; i++) {
+                System.out.println(perm[i]);
+            }
+        }
+
+        double score = 0.0;
+        for (int i = 0; i < N; i++) {
+            double dx = (double)(posX[perm[i]] - posX[perm[(i + 1) % N]]);
+            double dy = (double)(posY[perm[i]] - posY[perm[(i + 1) % N]]);
+            score += Math.sqrt(dx * dx + dy * dy);
+        }
+        
+        return score;
     }
 
     private int [] getPermutation () throws IOException {
-        
         StringBuffer sb = new StringBuffer();
         sb.append(N).append('\n');
         for (int i = 0; i < N; ++i) {
@@ -84,18 +124,34 @@ public class Tester {
         os.write(sb.toString().getBytes());
         os.flush();
 
-        int nRet = Integer.parseInt(br.readLine());
-        int [] ret = new int[nRet];
-        for (int i = 0; i < nRet; ++i) {
+        int [] ret = new int[N];
+        for (int i = 0; i < N; ++i) {
             ret[i] = Integer.parseInt(br.readLine());
         }
-
         return ret;
     }
 
     public Tester (String seed) {
-        generate(seed);
-        System.out.println(runTest());
+        if (exec != null) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                proc = rt.exec(exec);
+                os = proc.getOutputStream();
+                is = proc.getInputStream();
+                br = new BufferedReader(new InputStreamReader(is));
+                //new ErrorReader(proc.getErrorStream()).start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Score = " + runTest(seed));
+        if (proc != null) {
+            try { 
+                proc.destroy(); 
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            }
+        }
     }
 
     public static void main (String[] args) {
