124c08f41b4dde21cf68d7b02f66743825c637a0
kosakkun <kosakkun0918@gmail.com>
2018-03-30 06:19:49 +0900

    RPPのテンプレを作った．

---
+++ RPP/tester/Tester.java
@@ -0,0 +1,144 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import javax.swing.*;
+import javax.imageio.*;
+
+public class Tester {
+
+    public class Visualizer extends JPanel implements WindowListener {
+        
+        public void paint(Graphics g) {
+
+            try {
+            
+            } catch (Exception e) { 
+                e.printStackTrace();
+            }
+        }
+
+        public Visualizer () {
+            jf.addWindowListener(this);
+        }
+
+        public void windowClosing(WindowEvent e) {
+            if (proc != null) {
+                try { 
+                    proc.destroy();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            System.exit(0);
+        }
+
+        public void windowActivated(WindowEvent e) { }
+        public void windowDeactivated(WindowEvent e) { }
+        public void windowOpened(WindowEvent e) { }
+        public void windowClosed(WindowEvent e) { }
+        public void windowIconified(WindowEvent e) { }
+        public void windowDeiconified(WindowEvent e) { }
+
+    }
+
+    /********************************************************************/
+
+    JFrame jf;
+    Visualizer v;
+    InputStream is;
+    OutputStream os;
+    BufferedReader br;
+
+    static Process proc;
+    static String fileName, exec;
+    static boolean save, vis;
+
+    /********************************************************************/
+
+    public void generate (String seedStr) {
+
+        try {   
+
+        } catch (Exception e) {
+            //addFatalError("An exception occurred while generating test case.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public double runTest (String seed) {
+
+        try {
+            generate(seed);
+            
+            } catch (Exception e) {
+                //addFatalError("Failed to get result from permute.");
+                return -1;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+        if (vis) {
+            //jf.setSize((SIZE + 4) * 10, (SIZE + 6) * 10);
+            //jf.setVisible(true);
+        }
+        
+        return score;
+    }
+
+    private int [] getPermutation () throws IOException {
+        
+    }
+
+    public Tester (String seed) {
+        if (vis) {
+            jf = new JFrame();
+            v = new Visualizer();
+            jf.getContentPane().add(v);
+        }
+        if (exec != null) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                proc = rt.exec(exec);
+                os = proc.getOutputStream();
+                is = proc.getInputStream();
+                br = new BufferedReader(new InputStreamReader(is));
+                //new ErrorReader(proc.getErrorStream()).start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Score = " + runTest(seed));
+        if (proc != null) {
+            try { 
+                proc.destroy(); 
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        String seed = "1";
+        for (int i = 0; i < args.length; ++i) {
+            if (args[i].equals("-seed")) {
+                seed = args[++i];
+            } else if (args[i].equals("-exec")) {
+                exec = args[++i];
+            } else if (args[i].equals("-vis")) {
+                vis = true;
+            } else if (args[i].equals("-save")) {
+                save = true;
+            }
+        }
+        fileName = seed;
+        Tester test = new Tester(seed);
+    }
+
+}
