d4545837606c8755d36f5f994a32fcf5cf6060bb
kosakkun <kosakkun0918@gmail.com>
2018-05-08 14:33:36 +0900

    GCPのテンプレを用意した．
    グラフ彩色問題

---
+++ GCP/README.md
@@ -0,0 +1,28 @@
+# GCP（Graph Coloring Problem）
+
+## 問題文
+グラフの彩色問題です．
+
+## 制約
+
+### 入力
+
+### 出力
+
+## スコア
+
+## テスタ
+TopCoder の Marathon Match と同じです．```"<command>"```にプログラムの実行コマンド，```<seed>```に乱数のシードを入れてください．
+```sh
+$ java -jar tester.jar -exec "<command>" -seed <seed>
+```
+その他オプション
+```
+-vis  : ビジュアライズ
+-save : 画像の保存
+```
+
+
+## 目安 
+
+##  参考文献
---
+++ GCP/tester/Tester.java
@@ -0,0 +1,177 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import javax.swing.*;
+import javax.imageio.*;
+
+public class Tester {
+
+    public class Visualizer extends JPanel implements WindowListener {
+        
+        public void paint(Graphics g) {
+
+            try {
+                
+                if (save) {
+                    ImageIO.write(bi, "png", new File(fileName +".png"));
+                }
+
+            } catch (Exception e) { 
+                e.printStackTrace();
+            }
+        }
+
+        public Visualizer () {
+            jf.addWindowListener(this);
+        }
+
+        public void windowClosing(WindowEvent e) {
+            if (proc != null) {
+                try { 
+                    proc.destroy();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            System.exit(0);
+        }
+
+        public void windowActivated(WindowEvent e) { }
+        public void windowDeactivated(WindowEvent e) { }
+        public void windowOpened(WindowEvent e) { }
+        public void windowClosed(WindowEvent e) { }
+        public void windowIconified(WindowEvent e) { }
+        public void windowDeiconified(WindowEvent e) { }
+
+    }
+
+    /********************************************************************/
+
+    JFrame jf;
+    Visualizer v;
+    InputStream is;
+    OutputStream os;
+    BufferedReader br;
+
+    static Process proc;
+    static String fileName, exec;
+    static boolean save, vis;
+
+    final int MAXN = 1000, MINN = 50;
+    final int SIZE = 100 + 1;
+    final int VIS_SIZE = (SIZE + 3) * 10;
+    int N;
+    int [][] G;
+    int [] perm;
+
+    /********************************************************************/
+
+    public void generate (String seedStr) {
+
+        try {   
+            SecureRandom rnd = SecureRandom.getInstance("SHA1PRNG");
+            long seed = Long.parseLong(seedStr);
+            rnd.setSeed(seed);
+
+            N = rnd.nextInt(MAXN - MINN + 1) + MINN;
+         
+        } catch (Exception e) {
+            System.err.println("An exception occurred while generating test case.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public double runTest (String seed) {
+
+        try {
+            generate(seed);
+            if (proc != null) try {
+                perm = getPermutation();
+                
+            } catch (Exception e) {
+                System.err.println("Failed to get result from permute.");
+                return -1;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+
+        if (vis) {
+            jf.setSize(VIS_SIZE, VIS_SIZE);
+            jf.setVisible(true);
+        }
+
+        double score = -1;
+        
+        return score;
+    }
+
+    private int [] getPermutation () throws IOException {
+        StringBuffer sb = new StringBuffer();
+        sb.append(N).append('\n');
+        for (int i = 0; i < N; ++i) {
+            sb.append(posX[i]).append(' ');
+            sb.append(posY[i]).append('\n');
+        }
+        os.write(sb.toString().getBytes());
+        os.flush();
+
+        int [] ret = new int[N];
+        for (int i = 0; i < N; ++i) {
+            ret[i] = Integer.parseInt(br.readLine());
+        }
+        return ret;
+    }
+
+    public Tester (String seed) {
+        if (vis) {
+            jf = new JFrame();
+            v = new Visualizer();
+            jf.getContentPane().add(v);
+        }
+        if (exec != null) {
+            try {
+                Runtime rt = Runtime.getRuntime();
+                proc = rt.exec(exec);
+                os = proc.getOutputStream();
+                is = proc.getInputStream();
+                br = new BufferedReader(new InputStreamReader(is));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Score = " + runTest(seed));
+        if (proc != null) {
+            try { 
+                proc.destroy(); 
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        String seed = "1";
+        for (int i = 0; i < args.length; ++i) {
+            if (args[i].equals("-seed")) {
+                seed = args[++i];
+            } else if (args[i].equals("-exec")) {
+                exec = args[++i];
+            } else if (args[i].equals("-vis")) {
+                vis = true;
+            } else if (args[i].equals("-save")) {
+                save = true;
+                vis = true;
+            }
+        }
+        fileName = seed;
+        Tester test = new Tester(seed);
+    }
+
+}
