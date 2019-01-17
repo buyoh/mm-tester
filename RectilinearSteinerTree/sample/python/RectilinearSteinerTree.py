301a13cbb83cc5be91d8ca67f9b48670d99ffe24
kosakkun <kosakkun0918@gmail.com>
2019-01-18 02:56:39 +0900

    pythonとjavaのサンプルを修正した．

--- RectilinearSteinerTree/sample/python/RectilinearSteinerTree.py
+++ RectilinearSteinerTree/sample/python/RectilinearSteinerTree.py
@@ -1,15 +1,22 @@
 import sys
+from sets import Set
 
-N,M = map(int,raw_input().split())
-a = [];
-b = [];
+posX = [];
+posY = [];
+used = Set([])
 
-for i in range(M):
-	p = map(int,raw_input().split())
-	a.append(p[0])
-	b.append(p[1])
-
+N = int(raw_input())
 for i in range(N):
-    print i
+	p = map(int,raw_input().split())
+	posX.append(p[0])
+	posY.append(p[1])
+	used.add(p[0] * 100 + p[1])
+
+print (10000 - N)
+for x in range(100):
+	for y in range(100):
+		p = x * 100 + y
+		if p not in used:
+			print str(x) + " " + str(y)
 
 sys.stdout.flush()
