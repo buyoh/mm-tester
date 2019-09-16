79466b2f4eb61dbed735862bf09c2bf593487ddc
kosakkun <kosakkun0918@gmail.com>
2019-09-16 14:22:45 +0900

    Change dcp/sample to dcp/solver

---
+++ DiskCovering/solver/java/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+JAVAC="javac"
+TESTER="../../tester/Tester.jar"
+TESTNUM=10
+
+if [ -e *.class ]; then
+    rm *.class
+fi
+
+$JAVAC Main.java
+if [ $? -ne 0 ]; then
+    echo "Compilation failed."
+    exit 1
+fi
+
+for i in `seq 1 $TESTNUM`; do
+    java -jar $TESTER -exec "java Main" -seed $i -vis -save | { read rslt; echo "case $i: $rslt"; }
+done
